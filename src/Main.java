5e24cbbde7921fef808f93592e9194cec756017f
Joao <jpc.sousa@campus.fct.unl.pt>
2026-09-18 17:17:47 +0100

    fix da main constantes, in.nextLine() e break no switch

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String PHONE_EXISTS = "%s";
     public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
-    public static final String SHARED_PHONES = "There are contacts that share phone numbers";
+    public static final String SHARED_PHONES = "There are contacts that share phone numbers.";
     public static final String NO_SHARED_PHONES = "All contacts have different phone numbers";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
@@ -64,6 +64,7 @@ public class Main {
                     break;
                 case CHECK_SHARED_PHONES:
                     checkSharedPhones(cBook);
+                    break;
                 default:
                     System.out.println(COMMAND_ERROR);
             }
@@ -161,6 +162,7 @@ public class Main {
 
     private static void searchByPhone(Scanner in, ContactBook cBook) {
         int phone = in.nextInt();
+        in.nextLine();
         if (cBook.hasPhone(phone)) {
             String name = cBook.searchByPhone(phone);
             System.out.printf(PHONE_EXISTS, name);
